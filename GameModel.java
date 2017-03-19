7b6253517548e25fa11231d16af54cbbeb8b9152
Taha Khaldi <tahakhaldi@hotmail.com>
2017-03-19 09:54:18 -0400

    Update GameModel.java

--- GameModel.java
+++ GameModel.java
@@ -1,34 +1,8 @@
-// Author: Taha Khaldi 
-// Student numbers: 8667551 
-// Course: ITI 1121-D
-// Assignment: 3
-
 import java.util.Arrays;
 import java.util.Random;
 
-// ADD YOUR IMPORTS HERE
-
-/**
- * The class <b>GameModel</b> holds the model, the state of the systems. 
- * It stores the followiung information:
- * - the state of all the ``dots'' on the board (color, captured or not)
- * - the size of the board
- * - the number of steps since the last reset
- * - the current color of selection
- *
- * The model provides all of this informations to the other classes trough 
- *  appropriate Getters. 
- * The controller can also update the model through Setters.
- * Finally, the model is also in charge of initializing the game
- *
- * @author Guy-Vincent Jourdan, University of Ottawa
- */
 public class GameModel {
 
-
-	/**
-	 * predefined values to capture the color of a DotInfo
-	 */
 	public static final int COLOR_0           = 0;
 	public static final int COLOR_1           = 1;
 	public static final int COLOR_2           = 2;
@@ -38,7 +12,6 @@ public class GameModel {
 	public static final int NUMBER_OF_COLORS  = 6;
 
 
-	// ADD YOUR INSTANCE VARIABLES HERE
 	private int size;
 
 	private int curStepsNumber;
@@ -49,12 +22,6 @@ public class GameModel {
 
 	private final Random random = new Random ();
 
-	/**
-	 * Constructor to initialize the model to a given size of board.
-	 * 
-	 * @param size
-	 *            the size of the board
-	 */
 	public GameModel(int size) {
 
 		this.size = size;
@@ -62,11 +29,6 @@ public class GameModel {
 		reset();
 	}
 
-
-	/**
-	 * Resets the model to (re)start a game. The previous game (if there is one)
-	 * is cleared up . 
-	 */
 	public void reset(){
 
 		for (int row = 0; row < size; row ++)
@@ -81,123 +43,48 @@ public class GameModel {
 
 	}
 
-
-	/**
-	 * Getter method for the size of the game
-	 * 
-	 * @return the value of the attribute sizeOfGame
-	 */   
 	public int getSize(){
 
 		return size;
 	}
-
-	/**
-	 * returns the current color  of a given dot in the game
-	 * 
-	 * @param i
-	 *            the x coordinate of the dot
-	 * @param j
-	 *            the y coordinate of the dot
-	 * @return the status of the dot at location (i,j)
-	 */   
+	
 	public int getColor(int i, int j){
 
 		return model[i][j].getColor();
-	}
-
-	/**
-	 * returns true is the dot is captured, false otherwise
-	 * 
-	 * @param i
-	 *            the x coordinate of the dot
-	 * @param j
-	 *            the y coordinate of the dot
-	 * @return the status of the dot at location (i,j)
-	 */   
+	} 
 	public boolean isCaptured(int i, int j){
 
 		return model[i][j].isCaptured();
 	}
-
-	/**
-	 * Sets the status of the dot at coordinate (i,j) to captured
-	 * 
-	 * @param i
-	 *            the x coordinate of the dot
-	 * @param j
-	 *            the y coordinate of the dot
-	 */   
+ 
 	public void capture(int i, int j){
 
 		model[i][j].setCaptured(true);
-	}
-
-
-	/**
-	 * Getter method for the current number of steps
-	 * 
-	 * @return the current number of steps
-	 */   
+	}  
 	public int getNumberOfSteps(){
 
 		return curStepsNumber;
 	}
-
-	/**
-	 * Setter method for currentSelectedColor
-	 * 
-	 * @param val
-	 *            the new value for currentSelectedColor
-	 */   
+	
 	public void setCurrentSelectedColor(int val) {
 
 		this.currentSelectedColor = val;
 	}
-
-	/**
-	 * Getter method for currentSelectedColor
-	 * 
-	 * @return currentSelectedColor
-	 */   
+ 
 	public int getCurrentSelectedColor() {
 
 		return this.currentSelectedColor;
 	}
 
-
-	/**
-	 * Getter method for the model's dotInfo reference
-	 * at location (i,j)
-	 *
-	 * @param i
-	 *            the x coordinate of the dot
-	 * @param j
-	 *            the y coordinate of the dot
-	 *
-	 * @return model[i][j]
-	 */   
 	public DotInfo get(int i, int j) {
 
 		return model[i][j];
 	}
-
-
-	/**
-	 * The metod <b>step</b> updates the number of steps. It must be called 
-	 * once the model has been updated after the payer selected a new color.
-	 */
 	public void step(){
 
 		curStepsNumber ++;
 	}
 
-	/**
-	 * The metod <b>isFinished</b> returns true iff the game is finished, that
-	 * is, all the dats are captured.
-	 *
-	 * @return true if the game is finished, false otherwise
-	 */
 	public boolean isFinished(){
 
 		for (int row = 0; row < size; row ++)
@@ -212,13 +99,6 @@ public class GameModel {
 		}
 		return true;
 	}
-
-
-	/**
-	 * Builds a String representation of the model
-	 *
-	 * @return String representation of the model
-	 */
 	public String toString(){
 
 		return Arrays.deepToString(model);
