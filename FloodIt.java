046e33ce308ceaedc5dc63320af5964691b8d4da
Taha Khaldi <tahakhaldi@hotmail.com>
2017-03-19 09:50:31 -0400

    Update FloodIt.java

--- FloodIt.java
+++ FloodIt.java
@@ -1,24 +1,5 @@
-// Author: Taha Khaldi 
-// Student numbers: 8667551 
-// Course: ITI 1121-D
-// Assignment: 3
-
-/**
- * The class <b>FloodIt</b> launches the game
- *
- * @author Guy-Vincent Jourdan, University of Ottawa
- */
 public class FloodIt {
 
-
-	/**
-	 * <b>main</b> of the application. Creates the instance of  GameController 
-	 * and starts the game. If a game size (<12) is passed as parameter, it is 
-	 * used as the board size. Otherwise, a default value is passed
-	 * 
-	 * @param args
-	 *            command line parameters
-	 */
 	public static void main(String[] args) {
 
 		try
